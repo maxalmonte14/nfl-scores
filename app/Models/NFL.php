ad657d4e478617fc62dfe29dd9c6f3c4e45fe994
Max Almonte <maxalmonte14@hotmail.com>
2018-09-23 22:01:39 -0400

    Extracted HTTP client from it

--- app/Models/NFL.php
+++ app/Models/NFL.php
@@ -2,110 +2,39 @@
 
 namespace App\Models;
 
-use GuzzleHttp\Client;
+use \DateTime;
+use \DateTimeZone;
 use PHPCollections\Collections\GenericList;
+use App\Utilities\JSONParser;
+use App\Interfaces\HttpClientInterface;
 
+/**
+ * Manipulates NFL games related data.
+ */
 class NFL
 {
     /**
-     * HTTP client.
+     * The HTTP client for fetching remote data.
      *
-     * @var \GuzzleHttp\Client
+     * @var \App\Interfaces\HttpClientInterface
      */
     private $client;
 
     /**
-     * The remote data source.
+     * The current datetime,
+     * represents today's date.
      *
-     * @var string
+     * @var \DateTime
      */
-    private $url;
-
+    private $today;
+    
     /**
      * Initialize values.
      */
-    public function __construct()
+    public function __construct(HttpClientInterface $httpClient)
     {
-        $this->client = new Client();
-        $this->url = 'http://www.nfl.com/liveupdate/scores/scores.json';
-    }
-
-    /**
-     * Gets and decode data from
-     * the remote source.
-     * 
-     * @throws \GuzzleHttp\Exception\RequestException
-     * 
-     * @return array
-     */
-    public function getRemoteData(): array
-    {
-        $results = $this->client->request('GET', $this->url);
-
-        return json_decode($results->getBody(), true);
-    }
-
-    /**
-     * Return a collection of the games
-     * of this week.
-     * 
-     * @return \PHPCollections\Collections\GenericList
-     */
-    public function getWeekGames(): GenericList
-    {
-        $games = new GenericList(Game::class);
-
-        foreach ($this->getRemoteData() as $date => $gameData) {
-            $gameData['date'] = new \DateTime(substr($date, 0, -2));
-            $games->add(new Game($gameData));
-        }
-
-        return $games;
-    }
-
-    /**
-     * Return a collection of the games
-     * of today or null if there is no
-     * games for today.
-     * 
-     * @return \PHPCollections\Collections\GenericList|null
-     */
-    public function getTodayGames(): ?GenericList
-    {
-        return $this->getWeekGames()->filter(function ($key, $game) {
-            return $game->date->format('Y-m-d') == (new \DateTime())->format('Y-m-d');
-        });
-    }
-
-    /**
-     * Return a collection of the games
-     * that are currently being playing
-     * or null if there is no live games
-     * at the moment. 
-     * 
-     * @return \PHPCollections\Collections\GenericList|null
-     */
-    public function getLiveGames(): ?GenericList
-    {
-        return $this->getWeekGames()->filter(function ($key, $game) {
-            return !in_array($game->qtr, ['Final', 'Suspended', 'final overtime', null]);
-        });
-    }
-
-    /**
-     * Return a collection with one
-     * specific game or null if there's
-     * no game being played by that team.
-     * 
-     * @param string $team
-     * 
-     * @return \PHPCollections\Collections\GenericList|null
-     */
-    public function getLiveGameByTeam(string $team): ?GenericList
-    {
-        return $this->getWeekGames()->filter(function ($key, $game) use ($team) {
-            return $game->home['abbr'] === $team || $game->away['abbr'] === $team;
-        });
+        $this->client = $httpClient;
+        $this->today = new DateTime('now', new DateTimeZone('US/Eastern'));
     }
 
     /**
@@ -118,7 +47,7 @@ class NFL
     public function getFinishedGames(): ?GenericList
     {
         return $this->getTodayGames()->filter(function ($key, $game) {
-            return in_array($game->qtr, ['Final', 'final overtime']);
+            return in_array($game->score['phase'], ['FINAL', 'FINAL OVERTIME']);
         });
     }
 
@@ -134,7 +63,73 @@ class NFL
     public function getFinishedGameByTeam(string $team): ?GenericList
     {
         return $this->getFinishedGames()->filter(function ($key, $game) use ($team) {
-            return $game->home['abbr'] === $team || $game->away['abbr'] === $team;
+            return $game->gameSchedule['homeTeamAbbr'] === $team ||
+                $game->gameSchedule['visitorTeamAbbr'] === $team;
         });
+    }
+
+    /**
+     * Return a collection of the games
+     * that are currently being playing
+     * or null if there is no live games
+     * at the moment. 
+     * 
+     * @return \PHPCollections\Collections\GenericList|null
+     */
+    public function getLiveGames(): ?GenericList
+    {
+        return $this->getWeekGames()->filter(function ($key, $game) {
+            return !in_array($game->score['phase'], ['FINAL', 'SUSPENDED', 'FINAL OVERTIME', null]);
+        });
+    }
+
+    /**
+     * Return a collection with one
+     * specific game or null if there's
+     * no game being played by that team.
+     * 
+     * @param string $team
+     * 
+     * @return \PHPCollections\Collections\GenericList|null
+     */
+    public function getLiveGameByTeam(string $team): ?GenericList
+    {
+        return $this->getLiveGames()->filter(function ($key, $game) use ($team) {
+            return $game->gameSchedule['homeTeamAbbr'] === $team || 
+                $game->gameSchedule['visitorTeamAbbr'] === $team;
+        });
+    }
+
+    /**
+     * Return a collection of the games
+     * of today or null if there is no
+     * games for today.
+     * 
+     * @return \PHPCollections\Collections\GenericList|null
+     */
+    public function getTodayGames(): ?GenericList
+    {
+        return $this->getWeekGames()->filter(function ($key, $game) {
+            return str_replace('\/', '/', $game->gameSchedule['gameDate']) === $this->today->format('m/d/Y');
+        });
+    }
+
+    /**
+     * Return a collection of the games
+     * of this week.
+     * 
+     * @return \PHPCollections\Collections\GenericList
+     */
+    public function getWeekGames(): GenericList
+    {
+        $games = new GenericList(Game::class);
+        $data = $this->client->get($this->client::URL);
+        $parsedData = JSONParser::parse($data);
+
+        foreach ($parsedData['gameScores'] as $key => $gameData) {
+            $games->add(new Game($gameData));
+        }
+
+        return $games;
     }
 }
