c47b2bf6f5943c997d01447a6b1e6838ea774d4e
Max Almonte <maxalmonte14@hotmail.com>
2018-09-23 22:08:47 -0400

    Added test for NFLHttpClient class

---
+++ tests/Unit/NFLHttpClientTest.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Tests\Unit;
+
+use Tests\TestCase;
+use Tests\Fakes\FakeNFLHttpClient;
+
+class NFLHttpClientTest extends TestCase
+{
+    /** @test */
+    public function it_can_get_data_in_json_format()
+    {
+        $NFLHttpClient = new FakeNFLHttpClient();
+
+        $this->assertJson($NFLHttpClient->get(FakeNFLHttpClient::URL));
+    }
+}
